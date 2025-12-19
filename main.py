3162071a4df22ddc39182e2fd51a7a4f19c32c61
Quinticus <bquinte2007@gmail.com>
2025-12-19 09:29:31 -0500

    Added some logic
    Worked on this with Kenny in person.

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import random
 
-game_active = True
+game_active = True # Declaring this for the game loop later
+cash = 0
+consistent_wins = 0
+MAX_WIN_LIMIT = 5
+
+print("Welcome to Kenny's Casino")
+print("How much are we betting today?")
+betting_cash = int(input("Place the amount of cash: "))
+
+if betting_cash <= 0:
+  print("Let's give you some cash.")
+  betting_cash = 1000
+
